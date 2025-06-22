e0f96fc8996a2d2569a59f5d02269de5fa8af945
Your Name <you@example.com>
2025-06-22 16:40:52 +0300

    تعديل empid

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request) {
     }
 
     const user = {
-      id: records[0].fields.empid,
+      empid: records[0].fields.empid,
       email: records[0].fields.email,
       name: records[0].fields.name || '', 
     };
